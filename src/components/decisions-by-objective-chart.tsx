2270f1e5b2b72e2feaa4359ba53440d0d49e00d8
Blair Renwick <blairmrenwick@gmail.com>
2025-08-18 08:45:45 +0000

    The page load issue has been fixed. However, the horizontal bar chart is

--- src/components/decisions-by-objective-chart.tsx
+++ src/components/decisions-by-objective-chart.tsx
@@ -32,7 +32,7 @@ export function DecisionsByObjectiveChart({ decisions, objectives }: { decisions
       <CardContent>
         <ChartContainer config={chartConfig} className="min-h-64 w-full">
             <ResponsiveContainer width="100%" height={300}>
-              <BarChart data={data} layout="vertical" margin={{ left: 10, right: 20, top: 20, bottom: 20 }}>
+              <BarChart data={data} layout="vertical" margin={{ left: 120, right: 20, top: 20, bottom: 20 }}>
                 <CartesianGrid horizontal={false} />
                  <XAxis type="number" hide />
                  <YAxis
@@ -42,10 +42,9 @@ export function DecisionsByObjectiveChart({ decisions, objectives }: { decisions
                     axisLine={false}
                     tick={{
                         fontSize: 12,
-                        width: 150,
-                        textAnchor: 'start',
+                        fill: 'hsl(var(--muted-foreground))',
                     }}
-                    width={120}
+                    width={110}
                  />
                 <ChartTooltip
                   cursor={false}
